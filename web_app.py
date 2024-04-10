58725b821ecab0b13cef1654f806140c48aba373
Jacob Gottesman <115319553+jacobgottesman@users.noreply.github.com>
2024-04-10 15:17:09 -0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_aggrid import AgGrid
 import pandas as pd
 
 st.set_page_config(layout='wide')
@@ -7,14 +8,18 @@ st.set_page_config(layout='wide')
 def create_sidebar(tab):
     if tab == 'On-Off +/- Metrics w/ Season Stats':
         min_games = st.sidebar.slider('Select Minimum Number Games', 0, 15, 10)
+        min_o_points = st.sidebar.slider('Select Minimum Offensive Points Per Game', 0, 20, 0)
+        min_d_points = st.sidebar.slider('Select Minimum Defensive Points Per Game', 0, 20, 0)
         years = st.sidebar.selectbox('Select Seasons', ('All', '2021', '2022', '2023'))
         positions = st.sidebar.selectbox('Select Positions', ('All', 'Cutter', 'Handler', 'Defender'))
         teams = st.sidebar.selectbox('Select Team', tuple(team_options))
-        return min_games, years, positions, teams
+        return min_games, min_o_points, min_d_points, years, positions, teams
     elif tab == 'Mixed Effects Model Metrics w/ Career Stats':
         min_games1 = st.sidebar.slider('Select Minimum Number Games', 0, 100, 50)
+        min_o_points = st.sidebar.slider('Select Minimum Offensive Points Per Game', 0, 20, 0)
+        min_d_points = st.sidebar.slider('Select Minimum Defensive Points Per Game', 0, 20, 0)
         positions1 = st.sidebar.selectbox('Select Positions', ('All', 'Cutter', 'Handler', 'Defender'), key='x')
-        return min_games1, positions1
+        return min_games1, min_o_points, min_d_points, positions1
 
 @st.cache_data
 def data_upload():
@@ -60,7 +65,7 @@ with st.sidebar:
 # Display the selected tab
 if tab_selection == 'On-Off +/- Metrics w/ Season Stats':
     # tab1, tab2 = st.tabs(['On-Off +/- Metrics w/ Season Stats', 'Mixed Effects Model Metrics w/ Career Stats'])
-    min_games, years, positions, teams = create_sidebar(tab_selection)
+    min_games, min_o_points, min_d_points,  years, positions, teams = create_sidebar(tab_selection)
     
     # changing index to have name (year) pinned on left
     df1['Name (Year)']= df1.apply(lambda row: str(row['Name'] + ' ('+row['Year']+')'), axis =1)
@@ -70,14 +75,23 @@ if tab_selection == 'On-Off +/- Metrics w/ Season Stats':
 
     # with tab1:
     st.header("On-Off +/- Metrics w/ Season Stats")
-    st.dataframe(data=df1[(df1['Games'] >= min_games) &
+    st.dataframe(data=df1[(df1['Games'] >= min_games) & (df1['Offensive Points']>= min_o_points) & 
+                          (df1['Defensive Points'] >= min_d_points) &
                             ((positions == 'All') | (df1['Position'] == positions)) &
                             ((teams == 'All') | (df1['Team'] == teams))], width = 1500, height = 800)
+    
+    text = '''This project was created by Jacob Gottesman with the mentorship of Dr. Eric Gerber.  
+    The data in this project was gathered through the AUDL stats API along with the UFA webiste.  '''
+    st.markdown(text)
+    poster = "https://pbs.twimg.com/media/GK0wqkgaIAQmY-4?format=jpg&name=4096x4096"
+    st.markdown("View more info on this project: [poster](%s)" % poster)
+    linkedin = "https://www.linkedin.com/in/jacob-gottesman-neu/"
+    st.markdown("Linkedin: [link](%s)" % linkedin)
         
 
 elif tab_selection == 'Mixed Effects Model Metrics w/ Career Stats':
     # tab1, tab2 = st.tabs(['On-Off +/- Metrics w/ Season Stats', 'Mixed Effects Model Metrics w/ Career Stats'])
-    min_games1, positions1 =create_sidebar(tab_selection)
+    min_games1, min_o_points, min_d_points, positions1 =create_sidebar(tab_selection)
 
     # changing index to name to have that column as pinned left column
     df2.index = df2['Name']
@@ -85,12 +99,16 @@ elif tab_selection == 'Mixed Effects Model Metrics w/ Career Stats':
 
     # with tab2:
     st.header("Mixed Effects Model Metrics w/ Career Stats")
-    st.dataframe(data=df2[(df2['Games'] >= min_games1) &
+    st.dataframe(data=df2[(df2['Games'] >= min_games1) &( df2['Offensive Points']>= min_o_points) & 
+                          (df2['Defensive Points'] >= min_d_points) &
                         ((positions1 == 'All') | (df2['Position'] == positions1))], 
                         width = 1500, height = 800)
 
     text = '''This project was created by Jacob Gottesman with the mentorship of Dr. Eric Gerber.  
-    The data in this project was gathered through the AUDL stats API along with the UFA webiste.  
-    More info on this project and data can be found in the link below'''
+    The data in this project was gathered through the AUDL stats API along with the UFA webiste.  '''
     st.markdown(text)
-
+    poster = "https://pbs.twimg.com/media/GK0wqkgaIAQmY-4?format=jpg&name=4096x4096"
+    st.markdown("View more info on this project: [poster](%s)" % poster)
+    linkedin = "https://www.linkedin.com/in/jacob-gottesman-neu/"
+    st.markdown("Linkedin: [link](%s)" % linkedin)
+ 
